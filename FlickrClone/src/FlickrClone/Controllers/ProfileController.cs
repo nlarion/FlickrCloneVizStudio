7a3372382cb81420148ee67cc18687b22ebc9507
Neil Larion <neil.larion@gmail.com>
2016-04-27 14:33:48 -0700

    user can upload files from their computer.

--- FlickrClone/src/FlickrClone/Controllers/ProfileController.cs
+++ FlickrClone/src/FlickrClone/Controllers/ProfileController.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNet.Identity;
 using FlickrClone.Models;
 using Microsoft.Data.Entity;
 using System.Security.Claims;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using System.IO;
+using Microsoft.Net.Http.Headers;
 
 namespace FlickrClone.Controllers
 {
@@ -15,13 +19,15 @@ namespace FlickrClone.Controllers
     public class ProfileController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private IHostingEnvironment _env;
 
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public ProfileController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        public ProfileController(UserManager<ApplicationUser> userManager, ApplicationDbContext db, IHostingEnvironment env)
         {
             _userManager = userManager;
             _db = db;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -32,6 +38,21 @@ namespace FlickrClone.Controllers
                 .Where(x => x.User.Id == currentUser.Id);
 
             return View(theView);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ICollection<IFormFile> files)
+        {
+            var uploads = Path.Combine(_env.WebRootPath, "Images");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    await file.SaveAsAsync(Path.Combine(uploads, fileName));
+                }
+            }
+            return View();
         }
     }
 }
