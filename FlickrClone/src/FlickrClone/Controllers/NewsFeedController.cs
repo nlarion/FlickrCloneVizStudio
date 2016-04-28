dc7609baa5027f7f2975045fd09cd38e51cee0e7
michael smith <michaelanthonysmith00@gmail.com>
2016-04-28 13:31:38 -0700

    completed news feed.

---
+++ FlickrClone/src/FlickrClone/Controllers/NewsFeedController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using FlickrClone.Models;
+using Microsoft.Data.Entity;
+using System.Security.Claims;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using System.IO;
+using Microsoft.Net.Http.Headers;
+
+
+namespace FlickrClone.Controllers
+{
+    [Authorize]
+    public class NewsFeedController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public NewsFeedController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var newsFeed = _db.Photos
+                .Include(x => x.User)
+                .ToList();
+
+            return View(newsFeed);
+        }
+    }
+}
